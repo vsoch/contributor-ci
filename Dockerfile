bd0dc070f00916eec04843d9dd9d206dee96323a
vsoch <vsoch@users.noreply.github.com>
2022-06-24 13:01:11 -0600

    testing using new release of llnl-scraper
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM ubuntu:22.04
 RUN apt-get update && apt-get install -y python3 python3-pip git
 WORKDIR /code
 COPY . /code
-RUN pip3 install --upgrade pip && pip3 install -e . && \
-    pip3 install git+https://github.com/researchapps/scraper.git@update/python-support
+RUN pip3 install --upgrade pip && pip3 install -e .
 ENTRYPOINT ["/bin/bash", "/code/entrypoint.sh"]
--- setup.py
+++ setup.py
@@ -79,9 +79,6 @@ if __name__ == "__main__":
         extras_require={
             "all": [INSTALL_REQUIRES_ALL],
         },
-        dependency_links=[
-            "git+git://github.com/researchapps/scraper.git@update/python-support#egg=llnl-scraper",
-        ],
         classifiers=[
             "Intended Audience :: Science/Research",
             "Intended Audience :: Developers",
